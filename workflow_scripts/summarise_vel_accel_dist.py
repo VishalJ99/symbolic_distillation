38c76e79da33130fbb615ca684aedd406110d0f0
Vishal Jain <vj279@cam.ac.uk>
2024-06-05 14:30:39 +0100

    Add Flag to Use Raw .npy Files for summarise_vel_accel_dist.py
    Useful when the data has not been processed, i.e. train/test.py has not been
    run.

--- workflow_scripts/summarise_vel_accel_dist.py
+++ workflow_scripts/summarise_vel_accel_dist.py
@@ -53,6 +53,30 @@ def velocity_analysis(vel, accel, output_dir):
     plt.savefig(os.path.join(output_dir, "vel_accel_dist.png"))
 
 
+def load_data(data_dir, use_raw=False):
+    data_dir = os.path.join(data_dir, "processed" if not use_raw else "raw")
+
+    if use_raw:
+        raw_files = os.listdir(data_dir)
+        for f in raw_files:
+            if "accel" in f:
+                y = np.load(os.path.join(data_dir, f))
+            else:
+                x = np.load(os.path.join(data_dir, f))
+    else:
+        data_fname = [f for f in os.listdir(data_dir) if "process" in f][0]
+        graph_path = os.path.join(data_dir, data_fname)
+        graph = torch.load(graph_path)[0]
+        x = graph.x.numpy()
+        y = graph.y.numpy()
+
+    dim = (x.shape[-1] - 2) // 2
+    vel = x[:, dim:-2]
+    accel = y
+
+    return vel, accel
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Summarise velocities and accelerations"
@@ -61,10 +85,15 @@ if __name__ == "__main__":
         "root_data_dir", type=str, help="Base directory for processed data"
     )
     parser.add_argument("save_dir", type=str, help="Directory to save output")
-
+    parser.add_argument(
+        "--use_raw",
+        action="store_true",
+        help="Use raw data instead of processed data",
+    )
     args = parser.parse_args()
     root_data_dir = args.root_data_dir
     save_dir = args.save_dir
+    use_raw = args.use_raw
 
     experiments = ["spring", "r1", "r2", "charge"]
     dims = [2, 3]
@@ -74,20 +103,13 @@ if __name__ == "__main__":
         for dim in dims:
             for split in splits:
                 data_dir = os.path.join(
-                    root_data_dir, f"{experiment}_{dim}d/{split}/processed/"
+                    root_data_dir, f"{experiment}_{dim}d/{split}/"
                 )
-                data_fname = [
-                    f for f in os.listdir(data_dir) if "process" in f
-                ][0]
-                graph_path = os.path.join(data_dir, data_fname)
+                vel, accel = load_data(data_dir, use_raw=use_raw)
+
                 output_dir = os.path.join(
                     save_dir, f"{experiment}_{dim}", f"{split}"
                 )
 
-                graph = torch.load(graph_path)[0]
-                x = graph.x.numpy()
-                dim = (x.shape[-1] - 2) // 2
-                vel = x[:, dim:-2]
-                accel = graph.y.numpy()
                 print(f"[INFO] running analysis for {experiment}-{dim}-{split}")
                 velocity_analysis(vel, accel, output_dir)
